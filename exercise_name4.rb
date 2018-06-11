7eecc2bf4661d5246920ecf9652fbd75ab6c69b9
Rose Beazzy <rose.beatty@gmail.com>
2018-06-11 12:04:32 +0100

    Mad lib function version

--- exercise_name4.rb
+++ exercise_name4.rb
@@ -1,9 +1,11 @@
-puts "Enter a noun"
-noun = gets.chomp
-puts "Enter a verb"
-verb = gets.chomp
-puts "Enter an adjective"
-adjective = gets.chomp
-puts "Enter an adverb"
-adverb = gets.chomp
-"Do you #{verb} your #{adjective} #{noun} #{adverb}? Thats hilarious!"
+
+def my_method(word)
+    puts "Enter a" + word
+    return gets.chomp
+end
+     verb = my_method(+ " verb")
+     adjective = my_method(+ " adjective")
+     noun = my_method(+ " noun")
+     adverb = my_method(+ " adverb")
+    
+    puts "Do you #{verb} your #{adjective} #{noun} #{adverb}? Thats hilarious!"
