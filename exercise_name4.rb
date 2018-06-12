c1e41f5f9f0de942ba75631f3d752282e272f397
Rose Beazzy <rose.beatty@gmail.com>
2018-06-12 12:49:29 +0100

    mad lib game as a loop

--- exercise_name4.rb
+++ exercise_name4.rb
@@ -1,10 +1,21 @@
+more = true
+while more==true
+
 def my_method(word)
     puts "Enter a" + word
     return gets.chomp
 end
-     verb = my_method(+ " verb")
-     adjective = my_method(+ " adjective")
-     noun = my_method(+ " noun")
-     adverb = my_method(+ " adverb")
+     verb = my_method(" verb")
+     adjective = my_method(" adjective")
+     noun = my_method(" noun")
+     adverb = my_method(" adverb")
     
     puts "Do you #{verb} your #{adjective} #{noun} #{adverb}? Thats hilarious!"
+
+
+puts "Would you like to play again? Answer with yes or no"
+a = gets.chomp
+if a!="yes"
+    more = false
+  end
+end
